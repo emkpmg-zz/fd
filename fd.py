143e43456495632ce17049ace6dab33023bd6900
Deborah Akuoko <deborah.akuoko@postgrad.manchester.ac.uk>
2019-05-09 10:30:53 +0100

    #feature description

--- fd.py
+++ fd.py
@@ -14,8 +14,19 @@ from sklearn.model_selection import train_test_split
 
 fruitsData = pd.read_table('fruit_data_with_colors.txt')
 
+#summary of first 5 rows
+fruitsData.head()
+
+#summarized analyses of quantitative data
+fruitsData.describe()
+
 # create a dictionary of fruit type (fruit_label) which is numeric
 # and map to the actual fruit name (fruit_name). 
 #This gives a true representation of our output labels or anything we want to classify
 individualFruitNames = dict(zip(fruitsData.fruit_label.unique(), fruitsData.fruit_name.unique()))   
 
+#The fruitsData has information about fruit name (numeric and string), height, 
+# mass, color score and width of selected fruits.
+#heights -- how tall is the fruit 
+#widths -- how wide is the fruit
+#mass -- how heavy is the fruit
